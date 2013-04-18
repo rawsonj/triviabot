e7c1df0123a9411a265506fbedf5e4a9a171fdfc
Joe Rawson <joseph.rawson@gmail.com>
2013-04-18 15:31:34 -0700

    Wrote the answer class and removed those bits from functions.py.

--- functions.py
+++ functions.py
@@ -2,14 +2,6 @@
 # 
 # This is a trial file to test functions needed for the trivia bot.
 
-def obscureAnswer(answer):
-    oanswer = answer
-    for i in range(len(answer)):
-        if (oanswer[i] != '*'):
-            oanswer = oanswer.replace(oanswer[i], '*')
-    return oanswer
-
-
 
 if __name__ == "__main__":
     import random
