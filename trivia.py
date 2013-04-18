0acce78b8632ac858a9993c76fe94d5260fe3b81
Joe Rawson <joseph.rawson@gmail.com>
2013-04-18 16:28:38 -0700

    Merged answer.py into trivia.py and added functionality.
    getQuestion will take a filedescriptor as an argument and return
    a question and the answer for it.

--- trivia.py
+++ trivia.py
@@ -4,20 +4,71 @@
 #
 
 class Answer:
-    """
-    A class to handle answers.
+    ''' This class implements storage for an answer you want to conceil
+    and give clues 1 letter at a time.
+
+    Methods:
+
+    giveClue(): returns the masked string after revealing a letter and saving the mask.
+    getClue(): returns the masked string.
+    setAnswer('string'): makes this object reusable, sets a new answer and clue mask.
+    reveal(): returns the answer string. '''
     
-    It needs to take a string and save it, generate a mask to hide all of the letters,
-    randomly choose a character to unmask, show the masked clue, do this a few times,
-    and be able to show the answer on command.
-    """
-    clueletters = []
+    def __init__(self, answer):
+        self._answer = answer
+        self._maskedAnswer = str()
 
-    def __init__(self, string):
-        self.original = string[:]
-        self.masked = self.mask(self.original)
+        for i in self._answer:
+            if i == ' ':
+                self._maskedAnswer += ' '
+            else:
+                self._maskedAnswer += '*'
 
-def getQuestion(fd,fLen):
+    def giveClue(self):
+        from random import randint
+
+        letter = ' '
+
+        while letter == ' ':
+            index = randint(0, len(self._answer)-1)
+            letter = self._answer[index]
+            if self._maskedAnswer[index] == letter:
+                letter = ' '
+
+        temp = list(self._maskedAnswer)
+
+        temp[index] = letter
+
+        self._maskedAnswer = "".join(temp)
+
+        return self._maskedAnswer
+
+    def getClue(self):
+        return self._maskedAnswer
+
+    def setAnswer(self, answer):
+        self.__init__(answer)
+
+    def reveal():
+        return self._answer
+
+def getQuestion(self,fd):
+    from random import choice
+    lines = fd.read().splitlines()
+    myline = choice(lines)
+    [self._question, temp] = myline.split('*')
+    self._answer.setAnswer(temp)
     
 if __name__ == "__main__":
+
+    from os import walk
+
+    question = ""
     
+    # go get our list of questions
+    list_generator = walk('./questions/')
+    filelist = []
+    for i in list_generator:
+        filelist.append(i)
+    filelist = filelist[0][2]
+    #print(filelist)
