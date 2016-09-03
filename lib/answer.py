ae51ba2d6b24c903e2b06f6b6965627203d47430
Joe Rawson <joseph.rawson@gmail.com>
2016-09-02 22:35:37 -0700

    Issue 55 (#63)
    * maint: Cleaned up documentation since I didn't understand docutils when I wrote this.
    feat: Added a __len__ in case we want it eventually.
    
    * maint: Refactored give_clue to be less bad.
    feat: Implemented #55. Pushed for review.
    feat: Fixed #55

--- lib/answer.py
+++ lib/answer.py
@@ -1,45 +1,61 @@
-from random import randint
+from random import randrange
 
 class Answer:
     '''
-    This class implements storage for an answer you want to conceil
+    This class implements storage for an answer you want to conceal
     and give clues 1 letter at a time.
-
-    Methods:
-
-    give_clue(): returns the masked string after revealing a letter and saving the mask.
-    get_clue(): returns the masked string.
-    set_answer('string'): makes this object reusable, sets a new answer and clue mask.
-    reveal(): returns the answer string.
     '''
 
     def __init__(self, answer='None'):
         self._answer = answer
+	# TODO: This is a really inefficient way to go about doing this,
+	# but I can't think of a good way to do this right now.
         self._masked_answer = str()
+	self._unmasked = 0
 
-        for i in self._answer:
-            if i.isalnum():
+        for character in self._answer:
+            if character.isalnum():
                 self._masked_answer += '*'
             else:
-                self._masked_answer += i
+                self._masked_answer += character
 
     def give_clue(self):
+	'''
+	Returns the masked string after revealing a letter and saving the mask.
+
+	If an answer has only 1-2 characters in it, no clues are given.
+
+	If an answer has 3-4, 1 clue is given.
+
+	If an answer has 5-6, 2 clues are given.
+	'''
+	# If all letters are unmasked, just return it.
         if self._answer == self._masked_answer:
             return self._masked_answer
+	elif len(self) < 3:
+	    return self._masked_answer
+	elif len(self) < 5 and self._unmasked == 1:
+	    return self._masked_answer
+	elif len(self) < 7 and self._unmasked == 2:
+	    return self._masked_answer
 
+	# Choose the next letter to unmask.
+	# Start with something to get us into the while loop.
         letter = ' '
 
+	# If it's a symbol, choose a different letter.
         while not letter.isalnum():
-            index = randint(0, len(self._answer)-1)
+	    # We need an index.
+            index = randrange(0, len(self))
+	    # Get the letter at that index from the answer.
             letter = self._answer[index]
+	    # If the character was already unmasked, or is a symbol.
             if self._masked_answer[index] == letter:
                 letter = ' '
 
-        temp = list(self._masked_answer)
-
-        temp[index] = letter
-
-        self._masked_answer = "".join(temp)
+	# Strings are immutable, so need to split and replace.
+	self._masked_answer = self._masked_answer[:index] + letter + self._masked_answer[index + 1:]
+	self._unmasked += 1
 
         return self._masked_answer
 
@@ -47,9 +63,18 @@ class Answer:
         return self._masked_answer
 
     def set_answer(self, new_answer):
+	'''
+	Sets a new answer string for the next question to use.
+	'''
         self.__init__(answer=new_answer)
 
     def _reveal(self):
+	'''
+	Returns the unmasked answer string.
+	'''
         return self._answer
+
+    def __len__(self):
+	return len(self._answer)
 
     answer = property(_reveal)
